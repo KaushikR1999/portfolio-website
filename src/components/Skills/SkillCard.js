6b3f61342c9b9d60a5112132533df6e3bb528794
KaushikR1999 <kaushik05@hotmail.co.uk>
2023-07-17 22:54:40 +0800

    Adjusted sizing of icons & text on smaller screens

--- src/components/Skills/SkillCard.js
+++ src/components/Skills/SkillCard.js
@@ -5,8 +5,8 @@ export default function Skill(props) {
     return (
         <div className="group flex flex-col items-center">
 
-            <img className="w-20 h-20 transition duration-500 group-hover:scale-75" src={props.skill.skillLogo} alt={props.skill.skillName}></img>
-            <p className="opacity-0 transition duration-500 group-hover:opacity-100">{props.skill.skillName}</p>
+            <img className="w-12 h-12 md:w-20 md:h-20 transition duration-500 group-hover:scale-75" src={props.skill.skillLogo} alt={props.skill.skillName}></img>
+            <p className="text-xs md:text-base opacity-0 transition duration-500 group-hover:opacity-100">{props.skill.skillName}</p>
 
         </div>
     )
